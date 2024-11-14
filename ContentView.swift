db5597615c570863de9523d760c44b5cdb48569d
pcsishun <dekearthsa@hotmail.com>
2024-11-14 14:44:50 +0700

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+//import DetailView
+
+struct ContentView: View {
+    @State private var counting = 0
+    func haddleCount(){
+        counting += 1
+    }
+    var body: some View {
+        NavigationView{
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, worlddddddd!")
+                    .padding()
+                Text(String(counting)).padding(.bottom,30).padding(.top,10)
+                Button(action: haddleCount){
+                    Text("Count")
+                        .frame(height: 50)
+                        .frame(width:160)
+                }.background(Color.blue)
+                    .cornerRadius(8)
+                    .fontWeight(Font.Weight.bold)
+                    .foregroundColor(.white)
+                    .padding()
+
+                NavigationLink(destination: DetailView()) {
+                    Text("Go to Detail View")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                    
+                }
+            }
+            .padding()
+        }
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ basicApp.swift
@@ -0,0 +1,18 @@
+//
+//  basicApp.swift
+//  basic
+//
+//  Created by pcsishun on 10/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct basicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
